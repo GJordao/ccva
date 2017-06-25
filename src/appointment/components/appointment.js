5fa577027e9d289585362c4fd38f76c53aa09f45
gjordao <jordao.f.gustavo@gmail.com>
2017-06-25 17:57:07 +0100

    Add functionality to appointment screen
    - Add state params for each field
    - Add feedback container with styles
    - Change each field to update the state on text change
    - Add submit method to validate and send data to server and to clear fields

--- src/appointment/components/appointment.js
+++ src/appointment/components/appointment.js
@@ -5,6 +5,8 @@ import {
 import DatePicker from 'react-native-datepicker';
 // GLOBALS
 import COLORS from './../../GLOBALS/colors';
+// API
+import ApiHelper from './../../api';
 
 const styles = StyleSheet.create({
     container: {
@@ -31,6 +33,22 @@ const styles = StyleSheet.create({
     innedContainer: {
         marginTop: 20
     },
+    feedbackContainer: {
+        alignItems: 'center',
+        backgroundColor: COLORS.feedbackContainerBackground,
+        borderColor: COLORS.feedbackContainerBorder,
+        borderRadius: 3,
+        borderWidth: 1,
+        flexDirection: 'row',
+        height: 100,
+        justifyContent: 'center',
+        margin: 20,
+        padding: 10
+    },
+    feedBackText: {
+        color: COLORS.feedbackText,
+        fontSize: 14
+    },
     question: {
         fontSize: 26,
         paddingBottom: 5
@@ -52,9 +70,17 @@ const styles = StyleSheet.create({
 
 export default class Appointment extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.submit = this.submit.bind(this);
         this.state = {
-            date: "",
+            name: '',
+            mail: '',
+            phone: '',
+            appointmentType: 'visit',
+            date: '',
+            message: '',
+            showFeedback: false,
+            feedback: ''
         }
     }
 
@@ -76,6 +102,41 @@ export default class Appointment extends Component {
         );
     }
 
+    submit() {
+        if (this.state.name.length === 0 ||
+            this.state.mail.length === 0 ||
+            this.state.phone.length === 0 ||
+            this.state.date.length === 0 ||
+            this.state.message.length === 0) {
+            this.setState({
+                feedback: 'Todos os campos são obrigatórios',
+                showFeedback: true
+            });
+        } else {
+            const postData = {
+                name: this.state.name,
+                contact: this.state.phone,
+                mail: this.state.mail,
+                message: this.state.message,
+                date: this.state.date,
+                type_table: this.state.appointmentType,
+                type_id: 0
+            }
+            ApiHelper.postContent('MTM1NTY4Mjg4MQ/reservation/add', postData).then((message) => {
+                this.setState({
+                    showFeedback: true,
+                    feedback: message,
+                    name: '',
+                    phone: '',
+                    mail: '',
+                    message: '',
+                    date: '',
+                    appointmentType: 'visit'
+                });
+            });
+        }
+    }
+
     render() {
         return (
             <ScrollView style={styles.container}>
@@ -83,27 +144,46 @@ export default class Appointment extends Component {
                     <View style={styles.questionContainer}>
                         <Text style={styles.question}>Nome: </Text>
                         <View style={styles.questionInputContainer}>
-                            <TextInput style={styles.questionInput} />
+                            <TextInput
+                                value={this.state.name}
+                                onChangeText={(value) => { this.setState({ name: value }); }}
+                                underlineColorAndroid='transparent'
+                                style={styles.questionInput}
+                            />
                         </View>
                     </View>
                     <View style={styles.questionContainer}>
                         <Text style={styles.question}> Email: </Text>
                         <View style={styles.questionInputContainer}>
-                            <TextInput style={styles.questionInput} />
+                            <TextInput
+                                value={this.state.mail}
+                                onChangeText={(value) => { this.setState({ mail: value }); }}
+                                underlineColorAndroid='transparent'
+                                style={styles.questionInput}
+                            />
                         </View>
                     </View>
                     <View style={styles.questionContainer}>
                         <Text style={styles.question}> Telemóvel: </Text>
                         <View style={styles.questionInputContainer}>
-                            <TextInput style={styles.questionInput} />
+                            <TextInput
+                                value={this.state.phone}
+                                onChangeText={(value) => { this.setState({ phone: value }); }}
+                                underlineColorAndroid='transparent'
+                                style={styles.questionInput}
+                            />
                         </View>
                     </View>
                     <View style={styles.questionContainer}>
                         <Text style={styles.question}> Tipo de marcação: </Text>
                         <View style={styles.questionInputContainer}>
-                            <Picker>
-                                <Picker.Item label="Visita" value="java" />
-                                <Picker.Item label="Evento" value="js" />
+                            <Picker
+                                selectedValue={this.state.appointmentType}
+                                onValueChange={(selectedValue) => this.setState({ appointmentType: selectedValue })}
+                            >
+                                <Picker.Item label="Visita" value="visit" />
+                                <Picker.Item label="Evento" value="event" />
+                                <Picker.Item label="Serviço" value="service" />
                             </Picker>
                         </View>
                     </View>
@@ -114,10 +194,22 @@ export default class Appointment extends Component {
                     <View style={styles.questionContainer}>
                         <Text style={styles.question}>Mensagem:</Text>
                         <View style={styles.questionInputContainer}>
-                            <TextInput style={styles.questionInput} multiline={true} numberOfLines={5} />
+                            <TextInput
+                                value={this.state.message}
+                                onChangeText={(value) => { this.setState({ message: value }); }}
+                                style={styles.questionInput} multiline={true}
+                                numberOfLines={5}
+                                underlineColorAndroid='transparent'
+                            />
                         </View>
                     </View>
-                    <TouchableOpacity style={styles.button}>
+                    {
+                        this.state.showFeedback &&
+                        <View style={styles.feedbackContainer}>
+                            <Text style={styles.feedBackText}>{this.state.feedback}</Text>
+                        </View>
+                    }
+                    <TouchableOpacity style={styles.button} onPress={this.submit}>
                         <Text style={styles.buttonText}>Submeter</Text>
                     </TouchableOpacity>
                 </View>
